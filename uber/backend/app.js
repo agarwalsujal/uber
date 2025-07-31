3c75569f20b47dd9c5239b78546d8c4c40db694b
agarwalsujal <sujalagarwa26@gmai.com>
2025-07-31 11:48:53 +0530

    Add MongoDB connection and mongoose dependency

--- uber/backend/app.js
+++ uber/backend/app.js
@@ -3,13 +3,15 @@ import cors from 'cors';
 
 
 import express from 'express';
+import { connect } from 'mongoose';
+import connectDB from './db/db.js';
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-
+connectDB(); // Connect to MongoDB
 // Example route
 app.get('/', (req, res) => {
   res.send('Uber backend API');
