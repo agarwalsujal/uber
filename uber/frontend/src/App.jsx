ff2d90f26b49372b103badbc62ac940fc77a51d9
agarwalsujal <sujalagarwa26@gmai.com>
2025-08-01 14:28:21 +0530

    feat: add routing and user/captain authentication pages

--- uber/frontend/src/App.jsx
+++ uber/frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Route, Routes } from 'react-router-dom';
+import UserSignUp from './pages/UserSignUp';
+import UserLogin from './pages/UserLogin';
+import CaptainSignup from './pages/CaptainSignup';
+import Captainlogin from './pages/Captainlogin';
+import Home from './pages/Home';
 const App = () => {
   return (
-    <div>App</div>
+    <div className="">
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/user/signup" element={<UserSignUp />} />
+        <Route path="/user/login" element={<UserLogin />} />
+        <Route path="/captain/signup" element={<CaptainSignup />} />
+        <Route path="/captain/login" element={<Captainlogin />} />
+      </Routes>
+    </div>
   )
 }
 
